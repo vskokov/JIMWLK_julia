0daaef8bcc6fd31c45d907af89125857c8f16fba
vskokov <vladi@skokov.net>
2022-06-20 14:25:13 -0400

    Bug corrected  V[i,i] -> V[i,j]

--- initial_mv.jl
+++ initial_mv.jl
@@ -13,11 +13,11 @@ using Plots
 
 const mu² = 1.0
 
-l = 32
-N = 512
-a = l/N
+const l = 32
+const N = 512
+const a = l/N
 const Ny = 10
-const m² = 0.2
+const m² = 0.2^2
 const Nc=3
 
 const variance_of_mv_noise = sqrt(mu² / (Ny * a^2))
@@ -26,7 +26,7 @@ seed=abs(rand(Int))
 rng = MersenneTwister(seed);
 
 
-t=gellmann(Nc,skip_identity=false)/2
+const t=gellmann(Nc,skip_identity=false)/2
 t[9]=t[9]*sqrt(2/3)
 
 
@@ -39,7 +39,7 @@ function compute_field!(rhok)
 # Modifies the argument to return the field
     Threads.@threads for i in 1:N
         for j in 1:N
-            rhok[i,j] = a^2*rhok[i,j] / (a^2 * m² + 4.0 * sin(π*(i-1)/N)^2 + 4.0 * sin(π*(j-1)/N)^2)
+            @inbounds rhok[i,j] = a^2*rhok[i,j] / (a^2 * m² + 4.0 * sin(π*(i-1)/N)^2 + 4.0 * sin(π*(j-1)/N)^2)
             # factor of a^2 was removed to account for the normalization of ifft next
             # ifft computes sum / (lenfth of array) for each dimension
         end
@@ -55,8 +55,8 @@ function compute_local_fund_Wilson_line()
 
     Threads.@threads for i in 1:N
         for j in 1:N
-            A_arr[i,j] = zeros(ComplexF32,(Nc,Nc))
-            V[i,j] = zeros(ComplexF32,(Nc,Nc))
+            @inbounds A_arr[i,j] = zeros(ComplexF32,(Nc,Nc))
+            @inbounds V[i,j] = zeros(ComplexF32,(Nc,Nc))
         end
     end
 
@@ -68,14 +68,14 @@ function compute_local_fund_Wilson_line()
 
         Threads.@threads for i in 1:N
             for j in 1:N
-                A_arr[i,j] = A_arr[i,j] + A[i,j]*t[b]
+                @inbounds A_arr[i,j] = A_arr[i,j] + A[i,j]*t[b]
             end
         end
     end
 
     Threads.@threads for i in 1:N
         for j in 1:N
-            V[i,j] .= exp(1.0im.*A_arr[i,j])
+            @inbounds V[i,j] .= exp(1.0im.*A_arr[i,j])
         end
     end
     return V
@@ -89,7 +89,7 @@ function compute_path_ordered_fund_Wilson_line()
         tmp=compute_local_fund_Wilson_line()
         Threads.@threads for i in 1:N
             for j in 1:N
-                V[i,j] .= V[i,j]*tmp[i,j]
+                @inbounds V[i,j] = V[i,j]*tmp[i,j]
             end
         end
     end
@@ -99,7 +99,7 @@ end
 function V_components(V)
     V_comp=zeros(ComplexF32,Nc^2)
     for b in 1:Nc^2
-        V_comp[b]=2.0*tr(V*t[b])
+        @inbounds V_comp[b]=2.0*tr(V*t[b])
     end
     return V_comp
 end
@@ -111,7 +111,7 @@ function compute_field_of_V_components(V)
     Threads.@threads for b in 1:Nc^2
         for i in 1:N
             for j in 1:N
-                Vc[b,i,j]=2.0*tr(V[i,i]*t[b])
+                @inbounds Vc[b,i,j]=2.0*tr(V[i,j]*t[b])
             end
         end
     end
@@ -121,8 +121,8 @@ end
 function FFT_Wilson_components(Vc)
     Vk=zeros(ComplexF64, (Nc^2,N,N))
 
-    for b in 1:Nc^2
-        Vk[b,:,:] .= a^2 .* fft(Vc[b,:,:])
+    Threads.@threads for b in 1:Nc^2
+        @inbounds Vk[b,:,:] .= a^2 .* fft(Vc[b,:,:])
     end
 
     return Vk
@@ -146,7 +146,7 @@ end
 
 function dipole(Vk)
     Sk = zeros(ComplexF64, (N,N))
-    for i in 1:N
+    Threads.@threads for i in 1:N
         for j in 1:N
             Sk[i,j] = 0.5*sum(Vk[b,i,j]*conj(Vk[b,i,j]) for b in 1:Nc^2)/Nc
         end
@@ -160,27 +160,27 @@ end
 
 
 function bin_x(S)
-    Nbins=N
+    Nbins=N÷2
     Sb=zeros(Float32,Nbins)
     Nb=zeros(Float32,Nbins)
     for i in 1:N÷2
         for j in 1:N÷2
-            r=(i-1)*a+(j-1)*a
-            idx_r = floor(Int,r / (a))+1
+            r=sqrt(((i-1)*a)^2+((j-1)*a)^2)
+            idx_r = floor(Int,r / (2*a))+1
             if (idx_r<=Nbins)
                 Sb[idx_r]=Sb[idx_r]+real(S[i,j])
                 Nb[idx_r]=Nb[idx_r]+1
             end
         end
     end
-    return(Sb ./ Nb)
+    return(Sb ./ (1e-16.+Nb))
 end
 
 
-Sb=zeros(N)
+Sb=zeros(N÷2)
 
 #open("out.dat","w") do io
-    for event in 1:2
+    for event in 1:10
 
         V=compute_path_ordered_fund_Wilson_line()
         test(V)
@@ -198,6 +198,21 @@ Sb=zeros(N)
         #Printf.@printf(io, "\n")
     end
 #end
-x=collect(1:N)*a
-plot(x,Sb/2)
-plot!(x,exp.(-x.^2/60 .*log.(exp(1.0) .+   200.0./x)))
+x=collect(1:N÷2)*a*2
+plot(x,Sb/10)
+#plot!(x,exp.(-x.^2/60 .*log.(exp(1.0) .+   200.0./x)))
+
+dataCpp=readdlm("/Users/vskokov/Dropbox/Projects/2022/MV_Jack_X_check_Julia_x_check/out.dat",' ')
+plot!(dataCpp[:,1],dataCpp[:,2])
+
+dataCpp=readdlm("/Users/vskokov/Dropbox/Projects/2022/MV_Jack_X_check_Julia_x_check/out1.dat",' ')
+plot!(dataCpp[:,1],dataCpp[:,2])
+
+dataCpp=readdlm("/Users/vskokov/Dropbox/Projects/2022/MV_Jack_X_check_Julia_x_check/out2.dat",' ')
+plot!(dataCpp[:,1],dataCpp[:,2])
+
+dataCpp=readdlm("/Users/vskokov/Dropbox/Projects/2022/MV_Jack_X_check_Julia_x_check/out3.dat",' ')
+plot!(dataCpp[:,1],dataCpp[:,2])
+
+dataCpp=readdlm("/Users/vskokov/Dropbox/Projects/2022/MV_Jack_X_check_Julia_x_check/out4.dat",' ')
+plot!(dataCpp[:,1],dataCpp[:,2])
